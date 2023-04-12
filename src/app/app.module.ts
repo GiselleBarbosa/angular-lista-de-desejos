7b03d55d7b2821d2a140afa829eb0abf3a9740cc
GiselleBarbosa <g88barbosa@gmail.com>
2023-04-12 14:59:54 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CriarProdutoComponent } from './criar-produto/criar-produto.component';
+import { DetalheProdutoComponent } from './detalhe-produto/detalhe-produto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CriarProdutoComponent,
+    DetalheProdutoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/detalhe-produto/detalhe-produto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhe-produto',
+  templateUrl: './detalhe-produto.component.html',
+  styleUrls: ['./detalhe-produto.component.scss']
+})
+export class DetalheProdutoComponent {
+
+}
