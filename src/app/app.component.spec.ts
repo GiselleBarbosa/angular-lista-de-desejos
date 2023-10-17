6253fff33c6d1f6e13d874a4c9176d4d2173cde9
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-17 17:56:12 -0300

    modificado favicon e nome do projeto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,5 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'lista-de-produtos'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('lista-de-produtos');
-  });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('lista-de-produtos app is running!');
-  });
 });
